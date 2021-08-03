fbf2f1db3e70e4596f4033f93de16b52ebc9972f
kdavis440 <davistime4@gmail.com>
2021-08-03 11:54:15 -0500

    basketball

--- july19 Java/Basketball.java
+++ july19 Java/Basketball.java
@@ -1,8 +1,31 @@
 public class Basketball extends Storeitem {
 
-    public Basketball(float price, String name, String description, int itemNum) {
-        super(price, name, description, itemNum);
-        //TODO Auto-generated constructor stub
-    }
-    
+private String Date;
+private String Section;
+private String Seat;
+public Basketball(float price, String name, String description, int itemNum) {
+    super(price, name, description, itemNum);
+    //TODO Auto-generated constructor stub
+}
+public String date() {
+    return date; {
+    this.date = date;
+}
+
+public void settickets(String price) {
+    this.price = price;
+
+public void name(String name) {
+        this.name = name;    
+}
+
+public void Section(String section) {
+        this.section = section;
+}
+    public void seat(String seat) {
+        this.seat = seat;    
+}
+    public void itemNum(String itemnum) {
+        this.itemNum = itemNum;    
+}
 }
--- july19 Java/Computer.java
+++ july19 Java/Computer.java
@@ -37,4 +37,3 @@ public class Computer extends Storeitem {
     }
     
 }
-//hey
