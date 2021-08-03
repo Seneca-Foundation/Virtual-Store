5e1b1a8b912f1fa4526f1ade9441dce190458fcf
maverick4200 <joshnava2@gmail.com>
2021-08-03 14:28:27 -0500

    added mac

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        Computer tempcomp = new Computer("mac" ,"Red",500,"Apple", "M1 Chip,","2021" ,1);
+        System.out.println(tempcomp.GetData());
+
     }
 }
--- src/Main.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-import java.util.function.Supplier;
-public class Main {
-    public static void main(String args[]) {
-        //testing repo
-        
-    }
-}
