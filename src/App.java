95d35a745bc1534f65d67c8e55a007ad9f5e1b46
sarai-sansone <sarai.sansone@gmail.com>
2021-08-06 13:43:36 -0500

    slight grammatical mistake

--- src/App.java
+++ src/App.java
@@ -69,11 +69,11 @@ public class App
         allItems.get(25).setKeywords(new ArrayList<String>() {{ add("100 kWh");add("100 kWh of energy");add("$200");}}); 
         allItems.add(new Powerplant("150 kWh",250, "provides enough energy", 0.0, 27));
         allItems.get(26).setKeywords(new ArrayList<String>() {{ add("150 kWh");add("150 kWh of energy");add("$250");}}); 
-        allItems.add(new Powerplant("200 kWh of energy", 300, "provides enough energy", 0.0, 28));
+        allItems.add(new Powerplant("200 kWh", 300, "provides enough energy", 0.0, 28));
         allItems.get(27).setKeywords(new ArrayList<String>() {{ add("200 kWh");add("200 kWh of energy");add("$300");}});
-        allItems.add(new Powerplant("250 kWh of energy", 350, "provides enough energy", 0.0, 29));
+        allItems.add(new Powerplant("250 kWh", 350, "provides enough energy", 0.0, 29));
         allItems.get(28).setKeywords(new ArrayList<String>() {{ add("250 kWh");add("250 kWh of energy");add("$350");}}); 
-        allItems.add(new Powerplant("300 kwh of energy", 400, "provides enough energy", 0.0, 30));
+        allItems.add(new Powerplant("300 kwh", 400, "provides enough energy", 0.0, 30));
         allItems.get(29).setKeywords(new ArrayList<String>() {{ add("300kWh");add("300 kWh of energy");add("$400");}});
         
         //password
