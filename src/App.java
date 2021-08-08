e938c3da4d0d5f4b3aae3966c1e15c356e39fe6d
Warren Thompson <warrenjthompson@gmail.com>
2021-08-08 10:23:50 -0700

    Removed unused library

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.Collection;
+
 public class App 
 {
     public static void main(String[] args) throws Exception 
