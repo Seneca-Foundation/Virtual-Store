78648740bb0b75853552a4a0454ca417b70c1a87
kdavis440 <davistime4@gmail.com>
2021-08-09 13:32:13 -0500

    basketball changes

---
+++ src/test/java/com/senecafoundation/BasketballTest.java
@@ -0,0 +1,41 @@
+package com.senecafoundation;
+
+import jdk.jfr.Timestamp;
+
+public class BasketballTest {
+    
+
+private Basketball systemUnderTest;
+
+@BeforeEach
+void assignMonster() {
+    this.systemUnderTest = new Basketball (
+
+}
+
+
+
+
+
+
+
+
+@Test
+void testBasketballCreate() {
+
+
+}
+
+
+
+
+
+
+
+
+
+@Test
+void getData() {
+
+
+}
