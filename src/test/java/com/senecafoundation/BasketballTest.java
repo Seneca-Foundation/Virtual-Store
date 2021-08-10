afc7198e68cd14d6b45c2bc644df3a7ada96ecc6
kdavis440 <davistime4@gmail.com>
2021-08-10 09:56:25 -0500

    basketball

--- src/test/java/com/senecafoundation/BasketballTest.java
+++ src/test/java/com/senecafoundation/BasketballTest.java
@@ -1,5 +1,7 @@
 package com.senecafoundation;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import jdk.jfr.Timestamp;
 
 public class BasketballTest {
@@ -9,12 +11,14 @@ private Basketball systemUnderTest;
 
 @BeforeEach
 void assignMonster() {
-    this.systemUnderTest = new Basketball (
-
-}
-
-
-
+    this.systemUnderTest = new Basketball ("Chicago Bulls vs Phoenix Suns", "250""July 30", "109","ToughMatch", "30", "7" )
+    "ChicagoBulls vs PhoenixSuns",
+}   "250"
+    "July 30"
+    "109"
+    "Toughmatch"
+    "30"
+    "7"
 
 
 
@@ -22,7 +26,14 @@ void assignMonster() {
 
 @Test
 void testBasketballCreate() {
-
+    assertEquals("ChicagoBulls vs PhoenixSuns", this.systemUnderTest.name);
+    assertEquals("250, this.systemUnderTest.price);
+    assertEquals("Relasedate", this.systemUnderTest.Releasedate);
+    assertEquals("Section", this.systemUnderTest.Section);
+    assertEquals("Toughmatch", this.systemUnderTest.description);
+    assertEquals(30, this.systemUnderTest.weight);
+    assertEquals(7, this.systemUnderTest.itemNum);
+}
 
 }
 
