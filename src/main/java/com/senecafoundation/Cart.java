f4a8307624f40676e78b5d182a3d001bdcd8411d
sarai-sansone <sarai.sansone@gmail.com>
2021-08-12 11:14:04 -0500

    comment deleted, list protected

--- src/main/java/com/senecafoundation/Cart.java
+++ src/main/java/com/senecafoundation/Cart.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class Cart{
 
 
-    List<StoreItem> storeItemsToPurchase;
+    protected List<StoreItem> storeItemsToPurchase;
     protected double total;
     
     public Cart()
@@ -31,7 +31,6 @@ public class Cart{
         for (StoreItem item : storeItemsToPurchase)
         {
             System.out.println(item.name + " $" + df.format(item.price));
-            //System.out.println(String.format("{0,-45} | {1,-10}", item.name , "$" + item.price ));
             total = total + item.price;
         }
         System.out.println("--------------------------------------------------------");
