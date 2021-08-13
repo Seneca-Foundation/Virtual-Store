97547860a3d64a82cf6aecebe3abbfa15cc478c6
sarai-sansone <sarai.sansone@gmail.com>
2021-08-13 13:59:38 -0500

    unnecessary comment deleted

--- src/main/java/com/senecafoundation/App.java
+++ src/main/java/com/senecafoundation/App.java
@@ -3,7 +3,7 @@ package com.senecafoundation;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-//testing branch
+
 public class App 
 {
     public static void main( String[] args )
