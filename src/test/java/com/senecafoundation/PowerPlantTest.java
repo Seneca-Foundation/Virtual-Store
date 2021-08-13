37940fb8c1d643718d795c348ab73d4154cdcc03
sarai-sansone <sarai.sansone@gmail.com>
2021-08-13 15:16:42 -0500

    deleted white space

--- src/test/java/com/senecafoundation/PowerPlantTest.java
+++ src/test/java/com/senecafoundation/PowerPlantTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 
-
-
 public class PowerPlantTest {
     
     private Powerplant systemUnderTest;
