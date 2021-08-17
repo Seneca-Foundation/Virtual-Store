7e4bf1b5a8092388b018ff07d8dec86233ffedda
maverick4200 <joshnava2@gmail.com>
2021-08-17 10:43:37 -0500

    fixed constructor and whitespace

--- src/main/java/com/senecafoundation/UsedBooks.java
+++ src/main/java/com/senecafoundation/UsedBooks.java
@@ -1,15 +1,17 @@
+package com.senecafoundation;
+
 public class UsedBooks extends Book {
-    protected string Condition;
-    public UsedBooks(String name, double price, String author, String cover, String description, String Condition, double weight, int itemNum) {
+
+    protected String Condition;
+
+    public UsedBooks(String name, double price, String author, String cover, String description, String condition, double weight, int itemNum) {
         
     super(name, price, author, cover, description, weight, itemNum);
-    this.Condition = this.Condition;
+        this.Condition = condition;
     }
+
     public String GetData() {
         return "Title: " + name + ", Price: $" + price + ", Author: " + author + ", Cover: " + cover + ", Condition: " + Condition+ ", Weight: " + weight + ", Item Number: " + itemNum;
     }
-
-
-
 
 }
