2481c4768bcaac4091a7dc780e4bd98b05363427
sarai-sansone <sarai.sansone@gmail.com>
2021-08-17 12:20:43 -0500

    CreateCSV method

--- src/main/java/com/senecafoundation/CreateCSV.java
+++ src/main/java/com/senecafoundation/CreateCSV.java
@@ -1,6 +1,40 @@
 package com.senecafoundation;
-import java.io.File;
-import java.io.PrintWriter;
+import java.io.*;
+//import java.io.File;
+//import java.io.PrintWriter;
 public class CreateCSV {
-    //src/main/java/com/senecafoundation/Objects.csv
+    String file = "src/main/java/com/senecafoundation/Objects.csv";
+    BufferedReader reader = null;
+    String line = "";
+    public void convertToCSV(String[] data)
+    {
+        try 
+        {
+            reader = new BufferedReader(new FileReader(file));
+            while((line = reader.readLine()) != null)
+            {
+                String[] row = line.split("");
+                for(String index : row)
+                {
+                    System.out.printf("%-10s", index);
+                }
+                System.out.println();
+            }
+        }
+        catch(Exception e) 
+        {
+            e.printStackTrace();
+        }
+        finally 
+        {
+            try
+            {
+                reader.close();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
 }
