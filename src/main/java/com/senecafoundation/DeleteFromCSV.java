89e475b27113af8a782cd8badcd8270b40a9808e
kdavis440 <davistime4@gmail.com>
2021-08-18 11:08:11 -0500

    deletedfile

--- src/main/java/com/senecafoundation/DeleteFromCSV.java
+++ src/main/java/com/senecafoundation/DeleteFromCSV.java
@@ -3,3 +3,13 @@ package com.senecafoundation;
 public class DeleteFromCSV {
     //Add ability to delete objects from file
 }
+import java.io.File; 
+public static void main(String[] args) { 
+    File myObj = new File("filename.txt"); 
+    if (myObj.delete()) { 
+      System.out.println("Deleted the file: " + myObj.getName());
+    } else {
+      System.out.println("Failed to delete the file.");
+    } 
+  } 
+}
