9da56b583009bbf1e8fa102794ae1fd2dc1f128b
kdavis440 <davistime4@gmail.com>
2021-08-18 14:43:47 -0500

    changes made

--- src/main/java/com/senecafoundation/DeleteFromCSV.java
+++ src/main/java/com/senecafoundation/DeleteFromCSV.java
@@ -1,13 +1,23 @@
 package com.senecafoundation;
-import java.io.File; 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.Scanner; 
 public class DeleteFromCSV {
     //Add ability to delete objects from file
   public static void deleteFromCSV() { 
-      File myObj = new File("filename.txt"); 
-      if (myObj.delete()) { 
-        System.out.println("Deleted the file: " + myObj.getName());
-      } else {
-        System.out.println("Failed to delete the file.");
-      } 
-    } 
+     FileWriter fw = new FileWriter(tempFile,true);
+     BufferedWriter bw = new BufferedWriter(fw);
+     PrintWriter pw = new PrintWriter(bw);
+     
+     x = new Scanner (new File(filepath);
+     x.close();
+     pw.flush();
+     pw.close ();
+     File oldFile;
+    oldFile.delete();
+     File dump = new File (filepath);
+     File newFile;
+    newFile.renameTo(dump);
   }
