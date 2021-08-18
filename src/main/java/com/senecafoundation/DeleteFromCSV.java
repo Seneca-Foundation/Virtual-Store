a8cce537a9d713998b9a1f381ab22bb772c2123c
sarai-sansone <sarai.sansone@gmail.com>
2021-08-18 11:34:46 -0500

    method name fixed

--- src/main/java/com/senecafoundation/DeleteFromCSV.java
+++ src/main/java/com/senecafoundation/DeleteFromCSV.java
@@ -2,7 +2,7 @@ package com.senecafoundation;
 import java.io.File; 
 public class DeleteFromCSV {
     //Add ability to delete objects from file
-  public static void main(String[] args) { 
+  public static void deleteFromCSV() { 
       File myObj = new File("filename.txt"); 
       if (myObj.delete()) { 
         System.out.println("Deleted the file: " + myObj.getName());
