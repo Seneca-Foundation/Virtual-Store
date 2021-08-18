86b6126a9ebe01fb46ccd808c0ea9b1ebb66a0a6
sarai-sansone <sarai.sansone@gmail.com>
2021-08-18 11:33:00 -0500

    syntax errors fixed

--- src/main/java/com/senecafoundation/DeleteFromCSV.java
+++ src/main/java/com/senecafoundation/DeleteFromCSV.java
@@ -1,15 +1,13 @@
 package com.senecafoundation;
-
+import java.io.File; 
 public class DeleteFromCSV {
     //Add ability to delete objects from file
-}
-import java.io.File; 
-public static void main(String[] args) { 
-    File myObj = new File("filename.txt"); 
-    if (myObj.delete()) { 
-      System.out.println("Deleted the file: " + myObj.getName());
-    } else {
-      System.out.println("Failed to delete the file.");
+  public static void main(String[] args) { 
+      File myObj = new File("filename.txt"); 
+      if (myObj.delete()) { 
+        System.out.println("Deleted the file: " + myObj.getName());
+      } else {
+        System.out.println("Failed to delete the file.");
+      } 
     } 
-  } 
-}
+  }
