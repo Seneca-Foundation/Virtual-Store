31f8f1498108f2af528296559cddf2f74a467a52
kdavis440 <davistime4@gmail.com>
2021-08-19 10:08:43 -0500

    changes

--- src/main/java/com/senecafoundation/DeleteFromCSV.java
+++ src/main/java/com/senecafoundation/DeleteFromCSV.java
@@ -6,12 +6,18 @@ import java.io.PrintWriter;
 import java.util.Scanner; 
 public class DeleteFromCSV {
     //Add ability to delete objects from file
-  public static void deleteFromCSV() { 
+  public static void deleteFromCSV(String tempFile, String filepath) { 
+  File oldFile = new File(filepath);
+  File newFile = new File(tempFile);
+    try 
+    {
      FileWriter fw = new FileWriter(tempFile, true);
      BufferedWriter bw = new BufferedWriter(fw);
      PrintWriter pw = new PrintWriter(bw);
-     
-     x= new Scanner (new File(filepath);
+     Scanner x= new Scanner (new File(filepath);
+     x.useDelimiter(",")
+     while(x.hasNext())
+     {
      x.close();
      pw.flush();
      pw.close ();
