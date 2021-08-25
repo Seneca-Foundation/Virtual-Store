c0bc1b2f3437a25a6ce9d4e266579a15a2388c8f
sarai-sansone <sarai.sansone@gmail.com>
2021-08-25 14:36:09 -0500

    unnecessary comment del

--- src/main/java/com/senecafoundation/App.java
+++ src/main/java/com/senecafoundation/App.java
@@ -3,7 +3,6 @@ package com.senecafoundation;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.UUID;
 
 public class App 
 {
--- src/test/java/com/senecafoundation/CreateDataUnitTest.java
+++ src/test/java/com/senecafoundation/CreateDataUnitTest.java
@@ -2,8 +2,6 @@ package com.senecafoundation;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.UUID;
-
 //import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 
