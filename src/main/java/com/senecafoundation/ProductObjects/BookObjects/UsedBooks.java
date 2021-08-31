16f2ba872be6ba5016b33e9bc1971b4e33ccff83
sarai-sansone <sarai.sansone@gmail.com>
2021-08-31 16:24:53 -0500

    toString in book subclasses

--- src/main/java/com/senecafoundation/ProductObjects/BookObjects/UsedBooks.java
+++ src/main/java/com/senecafoundation/ProductObjects/BookObjects/UsedBooks.java
@@ -1,18 +1,29 @@
 package com.senecafoundation.ProductObjects.BookObjects;
 
-public class UsedBooks extends Book {
-
+public class UsedBooks extends Book 
+{
+    protected String condition;
     
-        protected String Condition;
-    
-        public UsedBooks(String name, double price, String author, String cover, String description, String condition, double weight) {
+    public UsedBooks(String name, double price, String author, String cover, String description, String condition, double weight) {
             
-        super(name, price, author, cover, description, weight);
-            this.Condition = condition;
-        }
-    
-        public String GetData() {
-            return "Title: " + name + ", Price: $" + price + ", Author: " + author + ", Cover: " + cover + ", Condition: " + Condition+ ", Weight: " + weight + ", Item Number: " + getID();
-        }
-    
+    super(name, price, author, cover, description, weight);
+        this.condition = condition;
     }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+    
+    public String GetData() {
+        return "Title: " + name + ", Price: $" + price + ", Author: " + author + ", Cover: " + cover + ", Condition: " + condition + ", Weight: " + weight + ", Item Number: " + getID();
+    }
+
+    @Override 
+    public String toString() {
+        return this.getClass().getSimpleName() + "," + this.getID() + "," + this.getName() + "," + String.valueOf(this.getPrice()) + "," + this.getAuthor() + "," + this.getCover() + "," +this.getDescription() + "," + this.getCondition() + "," + this.getWeight();
+    }   
+}
