346bc1321645189f8f825abd4fa70789aa5f08f3
maverick4200 <joshnava2@gmail.com>
2021-09-07 18:12:14 -0500

    Added unit/Integration tests for Pricechangers

--- src/test/java/com/senecafoundation/PriceChangersTests/RateTaxTest.java
+++ src/test/java/com/senecafoundation/PriceChangersTests/RateTaxTest.java
@@ -1,20 +1,44 @@
 package com.senecafoundation.PriceChangersTests;
 
-import com.senecafoundation.PriceChangers.RateDiscount;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyDouble;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import com.senecafoundation.ITextFormatter;
+import com.senecafoundation.StoreItemFormatter;
 import com.senecafoundation.PriceChangers.RateTax;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 public class RateTaxTest {
-    private RateDiscount systemUnderTest;
+    private RateTax systemUnderTest;
+    @Mock private transient ITextFormatter item;
 
     @BeforeEach
     void createRateTax() {
-        this.systemUnderTest = new RateDiscount();
+        MockitoAnnotations.openMocks(this);
+        when(item.getPrice()).thenReturn(5.00);
+        doNothing().when(item).setPrice(anyDouble());
+        this.systemUnderTest = new RateTax();
     }
     @Test
-    void testRateDiscount() {
-        //this.systemUnderTest
+    void testUnitRateTax() {
+        double priceReturned = this.systemUnderTest.ChangePriceToItem(item);
+        assertEquals(5.00, priceReturned); 
+    }
+    @Test
+    void testIntegrationRateTax() {
+        ArrayList<String> keywords = new ArrayList<String>();
+        keywords.add("test");
+        StoreItemFormatter realItem = new StoreItemFormatter("Test Item", 5.00, "This is a test", UUID.randomUUID(), keywords);
+        Double priceReturned = this.systemUnderTest.ChangePriceToItem(realItem);
+        assertEquals(5.1, priceReturned);
     }
 }
