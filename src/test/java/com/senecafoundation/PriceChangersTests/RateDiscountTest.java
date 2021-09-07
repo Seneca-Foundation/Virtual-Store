85d447b22a0ca20505adb385c33a08a01018cd43
maverick4200 <joshnava2@gmail.com>
2021-09-07 12:01:58 -0500

    Added unitTest and IntegrationTest
    Co-authored-by: Warren Thompson <warrenjthompson@gmail.com>
    Co-authored-by: kdavis440 <kdavis440@users.noreply.github.com>
    Co-authored-by: sarai-sansone <sarai-sansone@users.noreply.github.com>
    Co-authored-by: Psanchez4501 <Psanchez4501@users.noreply.github.com>

--- src/test/java/com/senecafoundation/PriceChangersTests/RateDiscountTest.java
+++ src/test/java/com/senecafoundation/PriceChangersTests/RateDiscountTest.java
@@ -1,19 +1,45 @@
 package com.senecafoundation.PriceChangersTests;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyDouble;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import com.senecafoundation.ITextFormatter;
+import com.senecafoundation.StoreItemFormatter;
 import com.senecafoundation.PriceChangers.RateDiscount;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 public class RateDiscountTest {
     private RateDiscount systemUnderTest;
+    @Mock private transient ITextFormatter item;
 
     @BeforeEach
     void createRateDiscount() {
+        MockitoAnnotations.openMocks(this);
+        when(item.getPrice()).thenReturn(10.00);
+        doNothing().when(item).setPrice(anyDouble());
         this.systemUnderTest = new RateDiscount();
     }
+
     @Test
-    void testRateDiscount() {
-        //this.systemUnderTest
+    void testUnitRateDiscount() {
+        double priceReturned = this.systemUnderTest.ChangePriceToItem(item);
+        assertEquals(10.00, priceReturned);
     }
-}
+
+    @Test
+    void testInegrationRateDiscount() {
+        ArrayList<String> keywords = new ArrayList<String>();
+        keywords.add("test");
+        StoreItemFormatter realItem = new StoreItemFormatter("Test Item", 10.00, "This is a test item", UUID.randomUUID(), keywords);
+        double priceReturned = this.systemUnderTest.ChangePriceToItem(realItem);
+        assertEquals(9.7, priceReturned);
+    }}
