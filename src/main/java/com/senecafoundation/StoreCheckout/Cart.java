3f377fa7c1bdb0953e3016c84d26f1903195a4e3
sarai-sansone <sarai.sansone@gmail.com>
2021-09-10 14:25:44 -0500

    Co-authored-by: kdavis440 <kdavis440@users.noreply.github.com> Co-authored-by: Warren Thompson <warrenjthompson@gmail.com> Co-authored-by: maverick4200 <maverick4200@users.noreply.github.com>

--- src/main/java/com/senecafoundation/StoreCheckout/Cart.java
+++ src/main/java/com/senecafoundation/StoreCheckout/Cart.java
@@ -25,13 +25,12 @@ public class Cart{
     }
 
     public String PrintItems() {
-        DecimalFormat df = new DecimalFormat("#.##");
+        DecimalFormat df = new DecimalFormat("#.00");
         df.setRoundingMode(RoundingMode.CEILING);
-        String topMessage = String.join("Thank you for shopping with us. Here are your items and total:\n",
-                    "--------------------------------------------------------\n",
-                    "Item, Price\n",
-                    "--------------------------------------------------------\n");
-        //String itemsToBuy;
+        String topMessage = String.join("\n", "Thank you for shopping with us. Here are your items and total:",
+                    "--------------------------------------------------------",
+                    "Item, Price",
+                    "--------------------------------------------------------");
         for (StoreItem item : storeItemsToPurchase)
         {
             StoreItemDataPrinter itemToPrint = new StoreItemDataPrinter(
@@ -45,14 +44,14 @@ public class Cart{
                                 new RateTax()
                             );
             
-            //itemstoBuy += String.join(itemToPrint.PrintDataForCart());
+            topMessage = topMessage + "\n"+itemToPrint.PrintDataForCart();
             total = total + itemToPrint.getPrice();
         }
-        String bottomMessage = String.join("\n--------------------------------------------------------\n",
+        String bottomMessage = String.join("\n","--------------------------------------------------------",
                     "Total w/ Tax: $"+df.format(total),
-                    "\nThank you for shopping with us! here's a discount code for next time 10OFF\n",  
-                    "Follow us on instagram @VirtualStore21\n");
-        String all = String.join(topMessage,bottomMessage);
+                    "Thank you for shopping with us! here's a discount code for next time 10OFF",  
+                    "Follow us on instagram @VirtualStore21");
+        String all = String.join("\n",topMessage,bottomMessage);
         return all;
     }
 }
